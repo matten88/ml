d0c42f607aa3fcfcd02848ea2a768844866bc51d
matten88 <60659609+matten88@users.noreply.github.com>
2020-02-04 16:25:42 +0100

    Add files via upload

---
+++ data.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb  4 13:38:08 2020
+
+@author: mathiaspedersen
+"""
+import numpy as np
+import pandas as pd
+
+data=pd.read_csv(r'/Users/mathiaspedersen/Desktop/Mappen over alle mapper/Skole/Dtu/Kandidat/1. Semester inkl januar/Machine learning and datamining/Data/shot_logs.csv')
+print(data)
